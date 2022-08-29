21d2ba87aac92b68d2de7fd80f983945011816bd
Gurpreet <gs109690@gmail.com>
2022-08-29 13:21:33 -0500

    Updated ICO contract

--- contracts/TestTokenIco.sol
+++ contracts/TestTokenIco.sol
@@ -1,36 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./Crowdsale.sol";
+import "./TestToken.sol";
 
-contract TestTokenIco is Crowdsale {
+contract TokenICO is Token {
+    address public owner;
+    address payable public deppositAddress;
+    uint256 tokenPrice = 0.0001 ether;
+    uint256 hardCap = 50 ether;
+    uint256 public raisedAmount;
 
-    uint256 private preIcoSupply;
-    
-    constructor(uint256 rate, address payable wallet, IERC20 token, uint256 icoSupply)  Crowdsale(rate, wallet, token) {
-
-        preIcoSupply = icoSupply;
-    }
-
-    function _preValidatePurchase(address beneficiary, uint256 weiAmount)
-		internal
-		view
-		override(Crowdsale, TimedCrowdsale)
-		onlyWhileOpen
-	{
-		super._preValidatePurchase(beneficiary, weiAmount);
-	}
-
-	/**
-	 * @dev Overrides parent behavior by transferring tokens from wallet.
-	 * @param beneficiary Token purchaser
-	 * @param tokenAmount Amount of tokens purchased
-	 */
-	function _deliverTokens(address beneficiary, uint256 tokenAmount)
-		internal
-		override(Crowdsale, AllowanceCrowdsale)
-	{
-		super._deliverTokens(beneficiary, tokenAmount);
-	}
-}
+    // ICO Start/End
+    uint256 public saleStart = block.timestamp;
+    uint256 public saleEnd = block.timestamp + 604800; // sale ends one week after starting
 }
